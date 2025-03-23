dea808074b99a141c0376b7b3e54a526eb96e43c
Reiz Daci <reiz.daci@outlook.com>
2025-03-23 17:55:18 +0100

    Initial commit:  Angular project structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { ais } from './modelsAI/aismodel';
+import { SubscriptionComponent } from './components/subscription/subscription.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, SidebarComponent, ChatWindowComponent,SubscriptionComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isPremiumUser: boolean = false;
+  selectedAi: any;
+  ais=[...ais]
+  
+
+  onAiSelected(ai: any) {
+    this.selectedAi = ai;
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    useMockData: true,
+    openaiApiKey: '', 
+    claudeApiKey: '',
+    geminiApiKey: '',
+    llamaApiKey: '',
+    mistralApiKey: ''
+  };
