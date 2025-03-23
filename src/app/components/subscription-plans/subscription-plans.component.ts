c54967636ba975f7135901fb5d0c768d8d56ef77
Reiz Daci <reiz.daci@outlook.com>
2025-03-23 17:56:52 +0100

    subscription-plans component

---
+++ src/app/components/subscription-plans/subscription-plans.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-plans',
+  standalone: true,
+  imports: [],
+  templateUrl: './subscription-plans.component.html',
+  styleUrl: './subscription-plans.component.css'
+})
+export class SubscriptionPlansComponent {
+
+}
